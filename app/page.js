57b65a413805e9edc33ed73faae0ce1db5a160a7
sakib-xrz <sakibxrz21@gmail.com>
2023-11-19 19:27:27 +0600

    Api kit ready

--- app/page.js
+++ app/page.js
@@ -1,6 +1,15 @@
-import Edit from "./components/Edit";
+"use client";
 
-export default function Home() {
+import { QueryClient } from "@tanstack/react-query";
+import Edit from "./components/Edit";
+import APIREQUEST from "./utilities/requests/APIRequest";
+
+export default async function Home() {
+  const queryClient = new QueryClient();
+  // await queryClient.prefetchQuery({
+  //   queryKey: ["blogs"],
+  //   queryFn: () => APIREQUEST.blog.getBlogs().then(({ data }) => data),
+  // });
   return (
     <main>
       <Edit />
